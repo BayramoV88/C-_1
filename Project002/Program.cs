fa5c57af23cf45b97b0d179190748d43ea3b6e8b
Vadim Bayramov <bayramovva@gmail.com>
2022-07-11 00:25:07 +0300

    Add Project002

---
+++ Project002/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number_2 = Console.ReadLine();
+
+if (number_1 == number_2 * number_2);
+{
+    Console.WriteLine("Является");
+}
+else
+{
+    Console.WriteLine("Не является");
+}
