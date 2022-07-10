9e565a30990d36f409882e285fd34d09da1a0467
Vadim Bayramov <bayramovva@gmail.com>
2022-07-11 01:50:45 +0300

    Add change to project002

--- Project002/Program.cs
+++ Project002/Program.cs
@@ -1,13 +1,13 @@
-﻿Console.WriteLine("Введите первое число");
+﻿Console.Write("Введите первое число ");
 int number_1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число");
-int number_2 = Console.ReadLine();
+Console.Write("Введите второе число ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
 
-if (number_1 == number_2 * number_2);
+if (number_1 == number_2 * number_2)
 {
-    Console.WriteLine("Является");
+    Console.WriteLine("Число " + number_2 + " является квадратом числа " + number_1);
 }
 else
 {
-    Console.WriteLine("Не является");
+    Console.WriteLine("Число " + number_2 + " не является квадратом числа " + number_1);
 }
